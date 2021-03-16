1859a12c0ad8561b2f9ba4bc2fa40857f672e39a
Ravi <ravigornal@caliperbusiness.com>
2021-03-16 17:37:25 +0530

    Updates

---
+++ precache-manifest.9cf59b8c594c2b1a5930a18507bc446c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf98e3cea36f6ac4c4aa25f837a8c56",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "48e6365300719d570ff6",
+    "url": "/React-Deploy/static/css/main.04919ba9.chunk.css"
+  },
+  {
+    "revision": "05dfa6abd0e9cc6c727b",
+    "url": "/React-Deploy/static/js/2.c34e4dfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Deploy/static/js/2.c34e4dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e6365300719d570ff6",
+    "url": "/React-Deploy/static/js/main.fb39f141.chunk.js"
+  },
+  {
+    "revision": "428e6da3a9134ecdae47",
+    "url": "/React-Deploy/static/js/runtime-main.14a2af33.js"
+  }
+]);
